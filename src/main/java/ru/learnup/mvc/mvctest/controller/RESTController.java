3bc582b8372d3754b7db318f6b736f59da95e590
иван <ivanbessoltsev@gmail.com>
2022-05-07 13:20:04 +0500

    update ActiveMQ

--- src/main/java/ru/learnup/mvc/mvctest/controller/RESTController.java
+++ src/main/java/ru/learnup/mvc/mvctest/controller/RESTController.java
@@ -1,122 +1,151 @@
-//package ru.learnup.mvc.mvctest.controller;
-//
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.access.annotation.Secured;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//import ru.learnup.mvc.mvctest.Service.AuthorsService;
-//import ru.learnup.mvc.mvctest.Service.BookService;
-//import ru.learnup.mvc.mvctest.Service.ClientService;
-//import ru.learnup.mvc.mvctest.Service.OrderService;
-//import ru.learnup.mvc.mvctest.entity.Authors;
-//import ru.learnup.mvc.mvctest.entity.Book;
-//import ru.learnup.mvc.mvctest.entity.Client;
-//import ru.learnup.mvc.mvctest.entity.Order;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//
-//@Controller
-//@RestController
-//@RequestMapping("/api")
-//@Secured({"ADMIN", "USER"})
-//public class RESTController {
-//
-//    //*******************Инжектим сервисы
-//    @Autowired
-//    private BookService bookService;
-//    @Autowired
-//    private AuthorsService authorsService;
-//    @Autowired
-//    private ClientService clientService;
-//    @Autowired
-//    private OrderService orderService;
-//
-//    //*********************прописываем маппинги******************
-//
-//    //---------------Просмотр всех книг
-//
-//    @GetMapping("/books")
-//    public List<Book> showAllBooks() {
-//        List<Book> allBooks = bookService.getAllBooks();
-//        return allBooks;
-//    }
-//
-//    //---------------Просмотр одной книги
-//    @GetMapping("/books/{id}")
-//    public Book getBook(@PathVariable int id) {
-//        Book book = bookService.getBookById(id);
-//        return book;
-//    }
-//
-//    //---------------Удаление книги
-//    @DeleteMapping("/books/{id}")
-//    @Secured("ADMIN")
-//    public void deleteBook(int id) {
-//        bookService.deleteBook(id);
-//    }
-//
-//    //---------------Сохранение книги
-//    @PostMapping("/books/")
-//    @Secured("ADMIN")
-//    public void saveBook(Book book) {
-//        bookService.saveBook(book);
-//    }
-//
-//    //---------------Удаление автора
-//    @DeleteMapping("/authors/{id}")
-//    @Secured("ADMIN")
-//    public void deleteAuthor(int id) {
-//        authorsService.deleteAuthor(id);
-//    }
-//
-//    //---------------Просмотр всех авторов
-//    @GetMapping("/authors")
-//    public List<Authors> showAllAuthors() {
-//        List<Authors> allAuthors = authorsService.getAllAuthors();
-//        return allAuthors;
-//    }
-//
-//    //---------------Сохранение автора
-//    @PostMapping("/authors")
-//    @Secured("ADMIN")
-//    public void saveAuthor(Authors author) {
-//        authorsService.saveAuthors(author);
-//    }
-//
-//
-//    @GetMapping("/clients")
-//    public List<Client> showAllClients() {
-//        List<Client> allClients = clientService.getAllClients();
-//        return allClients;
-//    }
-//
-//    @PutMapping("/clients")
-//    public Client updateClient(@RequestBody Client client) {
-//        clientService.saveClient(client);
-//        return client;
-//    }
-//
-//    @GetMapping("/orders")
-//    public List<Order> showAllOrder() {
-//        List<Order> allOrder = orderService.getAllOrder();
-//        return allOrder;
-//    }
-//
-//
-//    @GetMapping("/orders/")
-//    public List<Order> showAllOrder(@RequestParam("clientID") int id) {
-//        List<Order> allOrder = orderService.getAllOrder();
-//        List<Order> orderClient = new ArrayList<>();
-//        for (Order o: allOrder) {
-//           if (o.getClient().getClientID()==id) {
-//               orderClient.add(o);
-//           }
-//        }
-//        return orderClient;
-//    }
-//
-//
-//}
+package ru.learnup.mvc.mvctest.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ru.learnup.mvc.mvctest.Service.AuthorsService;
+import ru.learnup.mvc.mvctest.Service.BookService;
+import ru.learnup.mvc.mvctest.Service.ClientService;
+import ru.learnup.mvc.mvctest.Service.OrderService;
+import ru.learnup.mvc.mvctest.entity.Authors;
+import ru.learnup.mvc.mvctest.entity.Book;
+import ru.learnup.mvc.mvctest.entity.Client;
+import ru.learnup.mvc.mvctest.entity.Order;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@RestController
+@RequestMapping({"/api"})
+public class RESTController {
+    @Autowired
+    private BookService bookService;
+    @Autowired
+    private AuthorsService authorsService;
+    @Autowired
+    private ClientService clientService;
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+
+
+    @JmsListener(destination="superqueue")
+    public  void processMessage(String message) {
+        log.info("Order:  " + message);
+    }
+
+    @GetMapping(value = "/send/{message}", produces = "text/html")
+    public String sendMessage(@PathVariable String message) {
+        jmsTemplate.convertAndSend("superqueue", message);
+        return "done";
+    }
+
+
+
+
+
+    @GetMapping({"/welcome"})
+    public String welcome() {
+        String w = "HELLO!!! WELCOME!!!";
+        return w;
+    }
+
+    @GetMapping({"/books"})
+    public List<Book> showAllBooks() {
+        List<Book> allBooks = this.bookService.getAllBooks();
+        return allBooks;
+    }
+
+    @GetMapping({"/books/{id}"})
+    public Book getBook(@PathVariable int id) {
+        Book book = this.bookService.getBookById(id);
+        return book;
+    }
+
+    @DeleteMapping({"/books/{id}"})
+    @Secured("ROLE_ADMIN")
+    public void deleteBook(@PathVariable int id) {
+       bookService.deleteBook(id);
+    }
+
+    @PostMapping({"/books"})
+    @Secured("ROLE_ADMIN")
+    public void saveBook(@RequestBody Book book) {
+       bookService.saveBook(book);
+    }
+
+    @DeleteMapping({"/authors/{id}"})
+    @Secured("ROLE_ADMIN")
+    public void deleteAuthor(@PathVariable int id) {
+        authorsService.deleteAuthor(id);
+    }
+
+    @GetMapping({"/authors"})
+    public List<Authors> showAllAuthors() {
+        List<Authors> allAuthors = authorsService.getAllAuthors();
+        return allAuthors;
+    }
+
+    @PostMapping({"/authors"})
+    @Secured("ROLE_ADMIN")
+    public void saveAuthor(@RequestBody Authors author) {
+        authorsService.saveAuthors(author);
+    }
+
+    @GetMapping({"/clients"})
+    public List<Client> showAllClients() {
+        List<Client> allClients = clientService.getAllClients();
+        return allClients;
+    }
+
+    @PutMapping({"/clients"})
+    public Client updateClient(@RequestBody Client client) {
+        clientService.saveClient(client);
+        return client;
+    }
+
+    @GetMapping({"/orders"})
+    public List<Order> showAllOrder() {
+        List<Order> allOrder =orderService.getAllOrder();
+        return allOrder;
+    }
+
+    @GetMapping({"/orders/"})
+    public List<Order> showAllOrder(@RequestParam("clientID") int id) {
+        List<Order> allOrder = orderService.getAllOrder();
+        List<Order> orderClient = new ArrayList();
+        Iterator var4 = allOrder.iterator();
+
+        while (var4.hasNext()) {
+            Order o = (Order) var4.next();
+            if (o.getClient().getClientID() == (long) id) {
+                orderClient.add(o);
+            }
+        }
+
+        return orderClient;
+    }
+
+    @GetMapping({"/orders/client"})
+    public List<Order> showByClient(@RequestBody Client client) {
+        List<Order> allOrder =orderService.getAllOrder();
+        List<Order> orderByClient = new ArrayList<>();
+        for (Order o: allOrder) {
+            if (o.getClient().getClientID()==client.getClientID()) {
+                orderByClient.add(o);
+            }
+        }
+        return orderByClient;
+    }
+
+
+}
--- src/main/java/ru/learnup/mvc/mvctest/security/WebSecurityConfig.java
+++ src/main/java/ru/learnup/mvc/mvctest/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package ru.learnup.mvc.mvctest.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,13 +11,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("Ivan")
                 .password("1234")
                 .roles("USER")
+
                 .and()
                 .withUser("admin")
                 .password("admin")
@@ -25,11 +29,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/api/").hasRole("USER")
-                .antMatchers("/api/").hasRole("ADMIN")
-//                .antMatchers("/api").permitAll()
-                .and().formLogin();
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/welcome").hasAnyRole("USER","ADMIN")
+                .antMatchers("/api/**").hasAnyRole("USER", "ADMIN")
+           //     .antMatchers("/api/**").hasRole()
+                .and().formLogin().permitAll();
+
+
+//        .antMatchers("/api").permitAll()
+//                .and().formLogin();
     }
 
     @Bean
